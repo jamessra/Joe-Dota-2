cd5c3c4f9af0390ae26fbf8313e8157889b0cec7
jamessra <jamessejunra@gmail.com>
2017-07-19 16:37:59 -0400

    Update ijoosongdotahistory.py

--- ijoosongdotahistory.py
+++ ijoosongdotahistory.py
@@ -13,49 +13,32 @@ api = dota2api.Initialise("Enter Valve API Key")
 match_history = api.get_match_history(account_id=steamID64)
 
 #List of Match ID, Match Details, Hero Picks
-number_of_matches = len(match_history['matches'])
-match_id = []
-team = []
-hero_pick = []
+match_ids = []
+teams = []
+hero_picks = []
 deaths = []
-results = []
+match_results = []
 
-match_number = 0
-while match_number < number_of_matches:
-	match_id.append(match_history['matches'][match_number]['match_id'])
+for match_number in match_history['matches']:
+	match_ids.append(match_number['match_id'])
 	
-	#List of Match Details
-	match_details = api.get_match_details(match_id=match_id[match_number])
+	match_details = api.get_match_details(match_id=match_number['match_id'])
 	
-	#Isolate ijoosong's Match Details
-	player_position = 0
-	while player_position < 8:
-		if match_details['players'][player_position]['account_id'] == steamID3:
-			break
-		else:
-			player_position += 1
+	for player in match_details['players']:
+		if player['account_id'] == steamID3:
+			if player['player_slot'] < 100:
+				teams.append('Radiant')
+			else:
+				teams.append('Dire')
+			hero_picks.append(player['hero_name'])
+			deaths.append(player['deaths'])
 	
-	#ijoosong's Team
-	if match_details['players'][player_position]['player_slot'] < 100:
-		team.append('Radiant')
-	else:
-		team.append('Dire')
-	
-	#List of Hero Picks		
-	hero_pick.append(match_details['players'][player_position]['hero_name'])
-	
-	#Number of deaths
-	deaths.append(match_details['players'][player_position]['deaths'])
-	
-	#Results
 	if match_details['radiant_win'] == 1:
-		results.append('Radiant')
+		match_results.append('Radiant')
 	else:
-		results.append('Dire')
-	
-	match_number += 1
-
-df = pd.DataFrame(list(zip(match_id, team, hero_pick, deaths, results)), columns=['Match ID', 'Team', 'Hero Picked', 'Deaths', 'Results'])
+		match_results.append('Dire')
+			
+df = pd.DataFrame(list(zip(match_ids, teams, hero_picks, deaths, match_results)), columns=['Match ID', 'Team', 'Hero Picked', 'Deaths', 'Match Results'])
 df.to_csv('ijoosong.csv', index=False)
 
 print 'done'
