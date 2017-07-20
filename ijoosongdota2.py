31bcde48bc7946beca512bb4de856afa4dc028a6
jamessra <jamessejunra@gmail.com>
2017-07-19 22:26:04 -0400

    Update ijoosongdota2.py

--- ijoosongdota2.py
+++ ijoosongdota2.py
@@ -1,104 +1,67 @@
 import dota2api
-import json
 import time
-from datetime import datetime
 import urllib
 import smtplib
 
-#Joe's Steam ID
+#ijoosong's Steam ID
 steamID = 17854758
 steamID3 = 35709316
 steamID64 = 76561197995975044
 
+#Valve API Key
+api = dota2api.Initialise("Enter Valve API Key")
+
 while True:
 	#Current Time
-	c_Time = datetime.now()
-	print c_Time
-	current_Time = time.mktime(c_Time.timetuple())
+	current_epoch_time = time.time()
+	current_time = time.strftime('%m-%d %H:%M:%S', time.localtime(current_epoch_time))
 
-	#dota2api info
-	#dota2 API key
-	api = dota2api.Initialise("Enter Valve API Key")
+	#ijoosong's Match History
+	match_history = api.get_match_history(account_id=steamID64)
 
-	#get Joe's Dota Match History
-	hist = api.get_match_history(account_id=steamID64)
+	#Recent Match ID
+	match_id = match_history['matches'][0]['match_id']
 
-	#get Joe's Most Recent Match ID
-	recent_match_id = hist['matches'][0]['match_id']
+	#Match Details
+	match_details = api.get_match_details(match_id=match_id)
 
-	#get Match Details
-	match = api.get_match_details(match_id=recent_match_id)
+	#Game Start Time, Pre Game Duration, Game Duration, Total Time
+	start_time = match_details['start_time']
+	pre_game_duration = match_details['pre_game_duration']
+	duration = match_details['duration']
+	total_epoch_time = start_time + pre_game_duration + duration
+	total_local_time = time.strftime('%m-%d %H:%M:%S', time.localtime(total_epoch_time))
 
-	#get Joe's Solo MMR from OpenDota API
-	opendota_mmr_url = "https://api.opendota.com/api/players/" + str(steamID3)
-	opendota_mmr_response = urllib.urlopen(opendota_mmr_url)
-	opendota_mmr = json.loads(opendota_mmr_response.read())
+	#ijoosong's Details
+	for player in match_details['players']:
+		if player['account_id'] == steamID3:
+			if player['player_slot'] < 100:
+				team = 'Radiant'
+			else:
+				team = 'Dire'
+			hero_pick = player['hero_name']
+			death_count = player['deaths']
 
-	#get Joe's Warding from OpenDota API
-	opendota_wards_url = "https://api.opendota.com/api/matches/" + str(recent_match_id)
-	opendota_wards_response = urllib.urlopen(opendota_wards_url)
-	opendota_wards = json.loads(opendota_wards_response.read())
-
-	#isolate Joe's player information from his match
-	i = 0
-	while i < 8:
-		if match['players'][i]['account_id'] == steamID3:
-			break
-		else:
-			i+=1
-
-	#Most recent game timestamp
-	game_time = match['start_time']
-	local_time = time.strftime("%m/%d %H:%M:%S", time.localtime(game_time))
-
-	#Time since last game in hours
-	time_difference = str((current_Time - game_time)/3600)
-
-	#What team is Joe on?
-	team = match['players'][i]['player_slot']
-	if team < 100:
-		team = 'Radiant'
+	#Match Results		
+	if match_details['radiant_win'] == 1:
+		match_result = 'Radiant'
 	else:
-		team = 'Dire'
-
-	#What hero did Joe pick?
-	hero_picked = str(match['players'][i]['hero_name'])
-
-	#How many times did Joe die?
-	death_count = str(match['players'][i]['deaths'])
-
-	#How many wards did Joe place/use?
-	wards_placed = str(opendota_wards['players'][i]['obs_placed'])
-	wards_used = str(opendota_wards['players'][i]['observer_uses'])
-
-	#Result of the match
-	results = match['radiant_win']
-	if results == 'True':
-		results = 'Radiant'
-	else:
-		results = 'Dire'
-
-	#Joe's Solo MMR
-	mmr = str(opendota_mmr['solo_competitive_rank'])
-
-	#Print Test
-	print "Last Game Played: " + time_difference + " hours ago"
-	print "Game started on: " + local_time
-	print "Joe's team: " + team
-	print "Joe's hero: " + hero_picked
-	print "Joe's Deathcount: " + death_count
-	print "Wards Placed: " + wards_placed
-	print "Wards Used: " + wards_used
-	print "Winner: " + results
-	print "Joe's Solo MMR: " + mmr
-
+		match_result = 'Dire'
+	
+	print 'Current Time: ' + current_time
+	print 'Game Finished: ' + total_local_time
+	print 'Team: ' + team
+	print 'Hero Pick: ' + hero_pick
+	print 'Death Count: ' + str(death_count)
+	print 'Winner: ' + match_result
+	
 	#login gmail
 	server = smtplib.SMTP("smtp.gmail.com", 587)
 	server.starttls()
-	server.login('Enter Gmail Account', 'Enter Gmail Password')
+	server.login('Enter Email Address', 'Enter Email Password')
 
 	#Notifications
-	if time_difference < 0.5:
+	if (current_epoch_time - total_epoch_time)/3600 < 0.25:
 		#send email to text
-		server.sendmail('Enter Gmail Account', 'Enter Recipients Number', '\n' + 'Last game played: ' + time_difference + ' hours ago' + '\n' + 'Game started on: ' + local_time + '\n' + 'Joe\'s Team: ' + team + '\n' + 'Joe\'s hero: ' + hero_picked + '\n' + 'Joe\'s Deathcount: ' + death_count + '\n' + 'Winner: ' + results + '\n' + 'Joe\'s Solo MMR: ' + mmr)
-	time.sleep(1800 - time.time() % 1800)
+		server.sendmail('Enter Email Address', 'Enter Phone Number', '\n' + 'Game finished on: ' + total_local_time + '\n' + 'Joe\'s Team: ' + team + '\n' + 'Joe\'s Hero: ' + hero_pick + '\n' + 'Joe\'s Deathcount: ' + str(death_count) + '\n' + 'Winner: ' + match_result)
+	time.sleep(900 - time.time() % 900)
